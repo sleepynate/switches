1c0f0ca391728e886df820d454ebac7efc114e7b
Nathan Dotz <nathan.dotz@gmail.com>
2019-03-14 14:34:24 -0400

    Initial Commit

---
+++ switches/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  switches
+//
+//  Created by Nathan Dotz on 3/14/19.
+//  Copyright © 2019 Nathan Dotz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
